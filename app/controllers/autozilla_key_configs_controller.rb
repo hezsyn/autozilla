826eac80f2854619f356ad6a52d9bc08fe3fe42b
Brandon <jebster1988@hotmail.com>
2017-10-04 15:09:04 -0700

    Fixed issue with adding locations

--- app/controllers/autozilla_key_configs_controller.rb
+++ app/controllers/autozilla_key_configs_controller.rb
@@ -10,20 +10,18 @@ class AutozillaKeyConfigsController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
     @azk = AutozillaKeyConfig.find(params[:id])
-
     @azk.update(azk_params)
-    redirect_to
+
+    redirect_to clonezilla_versions_path
   end
 
   private
     def azk_params
-      params.require(:autozilla_key_config).permit(:btldr, :purpose, :kernal, :boot, :user_name, :union, :parameters_set1, :edd, :parameters_set2, :keyboardLayout, :ocs_prerun0, :ocs_prerun1, :ocs_prerun2, :ocs_prerun3, :ocs_prerun4, :ocs_live_run, :ocs_live_keymap, :ocs_live_batch, :locales, :vga, :ifname, :parameters_set3, :fetch, :toram, :live_media_path)
+      params.require(:autozilla_key_config).permit(:params_set, :flags, :location, :live_run, :live_keymap, :live_param, :live_batch, :prerun0, :postrun0, :prerun1, :postrun1, :prerun2, :postrun2, :prerun3, :postrun3, :prerun4, :postrun4)
     end
 
 end
